3aaf49a4a6afc8281c648a866284365517c29768
Pikuleva <yoana.pikuleva@gmail.com>
2024-09-28 14:42:52 +0300

    Add project files.

---
+++ HomeApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace HomeApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
